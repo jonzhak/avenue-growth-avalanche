e98920b2db4d58737f27f0f7510b2e5fc8242502
20y <20y@20yBook.local>
2021-03-24 12:49:52 +0300

    v

--- api/getToken.php
+++ api/getToken.php
@@ -3,25 +3,25 @@
 function getToken(){
      
      $url                = 'https://useavalanche.us.auth0.com/oauth/token';
-     $client_id          = 'NiwToJ2Mlkn4ubzbymnC18vM08WSXyb';
-     $client_secret      = '_BxwOPM0xfZ6n2OUch75BjuJ8kvvuOzSLzZtmoGp5rCVfW3neYIQkBaTnGmMLWDM';
 
      $data = array(
-          'client_id' => $client_id, 
-          'client_secret' => $client_secret,
-          'audience' => 'https://useavalanche.us.auth0.com/api/v2/',
-          'grant_type' => 'client_credentials'
+          'client_id'         => 'NiwToJ2Mlkn4ubzbymnC18vM08WSXyb5', 
+          'client_secret'     => '_BxwOPM0xfZ6n2OUch75BjuJ8kvvuOzSLzZtmoGp5rCVfW3neYIQkBaTnGmMLWDM',
+          'audience'          => 'https://useavalanche.us.auth0.com/api/v2/',
+          'grant_type'        => 'client_credentials'
      );
      
      $data_string = json_encode($data);
+     
      $ch=curl_init($url);
+     
      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
      curl_setopt($ch, CURLOPT_HEADER, false);
      curl_setopt($ch, CURLOPT_HTTPHEADER,
           array(
-               'Content-Type:application/json',
+               'Content-Type:application/json'
           )
      );
 
@@ -29,19 +29,18 @@ function getToken(){
      
      curl_close($ch);
      
-     $resp = array();
+     $resp = [];
+      
+     if($result){
+          $response = json_decode( $result );
+            
+            if(isset($response->token_type) && !empty($response->token_type) && isset($response->access_token) && !empty($response->access_token)){ 
+               $resp['token'] = $response->token_type." ".$response->access_token;
+            }
 
-     print_r($result);
-     
-     // if($result){
-     //      $response = json_decode( $result );
+     }
 
-     //      if(isset($response->token_type) && !empty($response->token_type) && isset($response->access_token) && !empty($response->access_token)){
-     //           $resp['token'] = $response->token_type." ".$response->access_token;
-     //      }
-     // }
-
-     // return $resp;
+      echo json_encode($resp);
 }
 
-if($_GET['getToken']){ getToken() };
+getToken();
