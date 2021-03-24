ce1b0bbf765cd6b59f6abee749e8754495747671
20y <20y@20yBook.local>
2021-03-24 12:05:24 +0300

    Update getToken.php

--- api/getToken.php
+++ api/getToken.php
@@ -1,42 +1,42 @@
 <?php 
 
 function getToken(){
-     // $url = 'https://useavalanche.us.auth0.com/oauth/token';
+     $url = 'https://useavalanche.us.auth0.com/oauth/token';
           
-     // $data = array(
-     //      'client_id' => 'NiwToJ2Mlkn4ubzbymnC18vM08WSXyb5', 
-     //      'client_secret' => '_BxwOPM0xfZ6n2OUch75BjuJ8kvvuOzSLzZtmoGp5rCVfW3neYIQkBaTnGmMLWDM',
-     //      'audience' => 'https://useavalanche.us.auth0.com/api/v2/',
-     //      'grant_type' => 'client_credentials'
-     // );
+     $data = array(
+          'client_id' => 'NiwToJ2Mlkn4ubzbymnC18vM08WSXyb5', 
+          'client_secret' => '_BxwOPM0xfZ6n2OUch75BjuJ8kvvuOzSLzZtmoGp5rCVfW3neYIQkBaTnGmMLWDM',
+          'audience' => 'https://useavalanche.us.auth0.com/api/v2/',
+          'grant_type' => 'client_credentials'
+     );
      
-     // $data_string = json_encode($data);
-     // $ch=curl_init($url);
-     // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-     // curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-     // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-     // curl_setopt($ch, CURLOPT_HEADER, false);
-     // curl_setopt($ch, CURLOPT_HTTPHEADER,
-     //      array(
-     //           'Content-Type:application/json',
-     //      )
-     // );
+     $data_string = json_encode($data);
+     $ch=curl_init($url);
+     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($ch, CURLOPT_HEADER, false);
+     curl_setopt($ch, CURLOPT_HTTPHEADER,
+          array(
+               'Content-Type:application/json',
+          )
+     );
 
-     // $result = curl_exec($ch);
+     $result = curl_exec($ch);
      
-     // curl_close($ch);
+     curl_close($ch);
      
-     // if($result){
-     //      $response = json_decode( $result );
+     $response = [];
 
-     //      if(isset($response->token_type) && !empty($response->token_type) && isset($response->access_token) && !empty($response->access_token)){
-     //           return $response->token_type." ".$response->access_token;
-     //      } else {
-     //           return 'something went wrong';
-     //      }
-     // }
+     if($result){
+          $response = json_decode( $result );
 
-     echo 'sdf';
+          if(isset($response->token_type) && !empty($response->token_type) && isset($response->access_token) && !empty($response->access_token)){
+               $response['token'] = $response->token_type." ".$response->access_token;
+          }
+     }
+
+     return json_encode($response);
 }
 
 if($_GET['getToken']){ getToken() };
